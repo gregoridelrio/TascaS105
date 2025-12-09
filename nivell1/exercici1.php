aadc51d26cf065eefd91cb09ff702336f679d410
Laurea Gregori <gregoridelrio@gmail.com>
2025-12-09 12:53:17 +0100

    fix: ejercici1 created objects gos and gat

--- nivell1/exercici1.php
+++ nivell1/exercici1.php
@@ -1,26 +1,39 @@
 <?php
-abstract class Animal {
+abstract class Animal
+{
     private $nom;
 
-    public function __construct($nom) {
+    public function __construct($nom)
+    {
         $this->nom = $nom;
     }
 
-    public function getNom() {
+    public function getNom()
+    {
         return $this->nom;
     }
 
     abstract public function parla();
 }
 
-class Gos extends Animal {
-    public function parla() {
+class Gos extends Animal
+{
+    public function parla()
+    {
         return "Guau!";
     }
 }
 
-class Gat extends Animal {
-    public function parla() {
+class Gat extends Animal
+{
+    public function parla()
+    {
         return "Miau!";
     }
 }
+
+$gos = new Gos("Josep");
+echo $gos->parla();
+
+$gat = new Gat("Maria");
+echo $gat->parla();
